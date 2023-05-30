657d8e57dacb5d6c990846b6d981cbede9fc60ee
Kundan-10 <kundanwakudkar10@gmail.com>
2023-05-30 13:38:25 +0530

    project succefully done

---
+++ EmployeeCrudApplication/src/com/kundan/Model/Main.java
@@ -0,0 +1,258 @@
+package com.kundan.Model;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+import com.kundan.sort.SortUsingName;
+
+public class Main {
+	
+	private static List<Employee> employees =new ArrayList<>();
+	
+	private static Scanner scanner=new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		int choice ;;
+		
+		do {
+			System.out.println("   MENU:-");
+			System.out.println("1. DISPLAY");
+			System.out.println("2. CREATE");
+			System.out.println("3. UPDATE");
+			System.out.println("4. DELETE");
+			System.out.println("5. SORT");
+			System.out.println("6. EXIT");
+			System.out.println("Enter Your Choice: ");
+			
+			choice=getIntInput();
+		
+			switch(choice) {
+			case 1:
+				displayEmployee();
+				break;
+				
+				
+			case 2:
+				createEmployee();
+				break;
+				
+			case 3:
+				updateEmployee();
+				break;
+				
+			case 4:
+				deleteEmployee();
+				break;
+				
+			case 5:
+				sortEmployee();
+				break;
+				
+			case 6:
+				System.out.println("Exit from Application");
+				break;
+				
+			default:
+				System.out.println("Invalid Choice. Choose Correct choice");
+			}
+			
+			
+			
+			
+			
+		} while (choice !=6);
+        
+		scanner.close();
+		
+
+	}
+
+	
+	
+	private static void displayEmployee() {
+		
+		if(employees.isEmpty()) {
+			System.out.println("No employee Found");
+			return;
+		};
+		
+		System.out.println("Employees:");   
+		
+		for(Employee employee:employees) {
+			System.out.println(employee);
+		}
+ 		
+		
+	}
+	
+	
+	private static void createEmployee() {
+		
+		System.out.println("CREATE Employee:");
+		
+	    System.out.println("Enter  Name:");
+			String name=scanner.next();
+		
+		System.out.println("Enter Gender:");
+		String gender=scanner.next();
+		
+		
+		
+	    System.out.println("Enter Email:");
+	    String email=scanner.next();
+		
+		System.out.println("Enter DateOfBirth:");
+	    String dateofBirth=scanner.next();
+		
+	
+	    System.out.println("Enter Phone:");
+	    String phone=scanner.next();
+	    
+	
+	    
+	    
+	    Employee employee=new Employee(name, gender, dateofBirth, email, phone);
+	    employees.add(employee);
+	    
+	    System.out.println("Employee added succesfully");
+		
+	}
+	
+
+	private static void updateEmployee() {
+		
+		System.out.println("Enter  phone of the employee to update");
+		String phoneUpdate=scanner.next();
+		
+		boolean match=false;
+		
+		for(Employee employee:employees) {
+			if(employee.getPhone().equals(phoneUpdate)) {
+				System.out.println("employee found . Enter new Details");
+				
+				
+			    System.out.println("Enter  Name:");
+				String name=scanner.next();
+				employee.setName(name);
+			
+				System.out.println("Enter Gender:");
+				String gender=scanner.next();
+				employee.setGender(gender);
+			
+			    System.out.println("Enter Email:");
+			    String email=scanner.next();
+			    employee.setEmail(email);
+			
+				System.out.println("Enter DateOfBirth:");
+			    String dateofBirth=scanner.next();
+			    employee.setDateofbirth(dateofBirth);
+			
+			
+			    System.out.println("Enter Phone:");
+			    String phone=scanner.next();
+			    employee.setPhone(phoneUpdate);
+		    
+			    System.out.println("Employee updated succesfully.");
+			    
+			    match=true;
+			    break;
+			}
+		}
+		
+		if(!match) {
+			System.out.println("Employee not found.");
+		}
+		
+	}
+	
+	
+
+	private static void deleteEmployee() {
+		
+		if(employees.isEmpty()) {
+			System.out.println("NO Employee found");
+			return;
+		}
+		
+		System.out.println("Enter the phone of the employee to delete");
+		String phone=scanner.next();
+		
+		boolean delete=false;
+		
+		for(Employee employee:employees) {
+			if(employee.getPhone().equals(phone)) {
+				employees.remove(employee);
+				System.out.println("Employee Remove Succesfully");
+				
+				delete=true;
+				break;
+			}	
+		}
+		
+		if(!delete) {
+			System.out.println("Employee not Found");
+		}
+
+	}
+	
+	
+
+	
+	private static void sortEmployee() {
+		if(employees.isEmpty()) {
+			
+			System.out.println("Employee Notfound");
+			return;
+		}
+		
+		
+		System.out.println("Sorting employees:");
+		
+		System.out.println("1. Sort By Name");
+		
+		System.out.println("Enter your choice:");
+		
+		int choice=scanner.nextInt();
+		
+		switch(choice) {
+		case 1:
+			
+			Collections.sort(employees,new SortUsingName());
+			
+			for(Employee emp:employees) {
+				System.out.println(emp.toString());
+			}
+			System.out.println("Employee sort by Name successfully.");
+			break;
+	
+		
+	}
+
+}
+	
+	private static int getIntInput() {
+		
+	    int input;
+	    while (true) {
+	        try {
+	            input = scanner.nextInt();
+	            scanner.nextLine(); 
+	            break;
+	        }
+	        catch (InputMismatchException e) {
+	            scanner.nextLine(); 
+	            System.out.print("Invalid input. Please enter a number: ");
+	           }
+	    }
+	        
+	        return input;
+
+	}
+	
+
+}
